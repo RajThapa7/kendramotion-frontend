539791688185c34a14938e7811f8cdc0479ad7b0
RajThapa7 <jyangoraj123@gmail.com>
2023-05-17 15:58:08 +0545

    feedback and map section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import VideoLibrary from "./features/video-library/VideoLibrary/VideoLibrary";
 import AppLayout from "./layouts/AppLayout/AppLayout";
 import Footer from "./components/Footer/Footer";
 import HeroSection from "./features/hero-section/HeroSection/HeroSection";
+import FeedbackandMapSection from "./features/FeedbackandMapSection/FeedbackandMapSection";
 
 function App() {
   return (
     <AppLayout>
       <HeroSection />
       <VideoLibrary />
+      <div className="w-full h-[1px] mt-12 mb-0 bg-gray-400 bg-black rounded-full"></div>
+      <FeedbackandMapSection />
       <Footer />
     </AppLayout>
   );
