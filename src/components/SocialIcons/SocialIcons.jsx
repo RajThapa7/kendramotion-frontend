a45966b242487bf502e71c6fa14b2470f2c787e5
RajThapa7 <jyangoraj123@gmail.com>
2023-05-17 14:39:52 +0545

    Social Icons modified

--- src/components/SocialIcons/SocialIcons.jsx
+++ src/components/SocialIcons/SocialIcons.jsx
@@ -2,10 +2,15 @@
 // eslint-disable-next-line react/prop-types
 export default function SocialLinks({ data }) {
   return (
-    <div className="inline-flex gap-x-6">
-      {data.map(({ icon, link, id }) => (
+    <div className="inline-flex gap-x-6 w-fit">
+      {data.map(({ img, link, id }) => (
         <a href={link} target="_blank" key={id} rel="noreferrer">
-          <p className="transition-smooth text-4xl hover:scale-110">{icon}</p>
+          <div className="object-contain w-12 rounded-full shadow-lg shadow-gray-400">
+            <img
+              className="transition-smooth text-4xl hover:scale-110 rounded-full"
+              src={img}
+            />
+          </div>
         </a>
       ))}
     </div>
