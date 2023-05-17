7f2d125db0dd51f9e1214700a401a85e9314e606
Prajwal Adhikari <prajwalad101@gmail.com>
2023-05-17 14:37:57 +0545

    feat: create seperate hero section component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import "./App.css";
 import VideoLibrary from "./features/video-library/VideoLibrary/VideoLibrary";
 import AppLayout from "./layouts/AppLayout/AppLayout";
 import Footer from "./components/Footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
-import BackgroundImg from "./assets/kendra.jpg";
+import HeroSection from "./features/hero-section/HeroSection/HeroSection";
 
 function App() {
   return (
     <AppLayout>
-      <img
-        src={BackgroundImg}
-        alt="background-img"
-        className="absolute left-0 right-0 w-full h-[80vh] object-cover"
-      />
-      <Navbar />
-      {/* Creates spacing*/}
-      <div className="h-[75vh]" />
+      <HeroSection />
       <VideoLibrary />
       <Footer />
     </AppLayout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-section": "url('/src/assets/kendra.jpg')",
+      },
+    },
   },
   plugins: [],
+  // set background image url
 };
