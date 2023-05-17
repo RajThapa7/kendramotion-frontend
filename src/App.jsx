9defca07a486fdd495622e33ca12fa77c839b00e
Prajwal Adhikari <prajwalad101@gmail.com>
2023-05-17 16:51:22 +0545

    feat: render components into home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import VideoLibrary from "./features/video-library/VideoLibrary/VideoLibrary";
 import AppLayout from "./layouts/AppLayout/AppLayout";
 import Footer from "./components/Footer/Footer";
 import HeroSection from "./features/hero-section/HeroSection/HeroSection";
+// import AudioLibrary from "./features/audio-library/AudioLibrary/AudioLibrary";
+import MovieLibrary from "./features/movies-library/MovieLibrary/MovieLibrary";
+import LocationFeedback from "./features/location&feedback/LocationFeedback/LocationFeedback";
 
 function App() {
   return (
     <AppLayout>
       <HeroSection />
       <VideoLibrary />
+      {/* <AudioLibrary /> */}
+      <MovieLibrary />
+      <div className="border-b border-black w-full mb-10" /> {/* Divider */}
+      <LocationFeedback />
       <Footer />
     </AppLayout>
   );
