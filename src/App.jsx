c73529163834b8416dd61783b1157d8ec3d3fec3
Prajwal Adhikari <prajwalad101@gmail.com>
2023-05-18 21:26:35 +0545

    feat: implement UI for audio-library

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,16 @@ import VideoLibrary from "./features/video-library/VideoLibrary/VideoLibrary";
 import AppLayout from "./layouts/AppLayout/AppLayout";
 import Footer from "./components/Footer/Footer";
 import HeroSection from "./features/hero-section/HeroSection/HeroSection";
-// import AudioLibrary from "./features/audio-library/AudioLibrary/AudioLibrary";
+import AudioLibrary from "./features/audio-library/AudioLibrary/AudioLibrary";
 import MovieLibrary from "./features/movies-library/MovieLibrary/MovieLibrary";
 import FeedbackAndMapSection from "./features/feedback-and-map/FeedbackAndMapSection/FeedbackAndMapSection";
-import ArtistProfileSection from "./features/ArtistProfileSection/ArtistProfileSection";
-import LatestWorkSection from "./features/LatestWorkSection/LatestWorkSection";
-import WantToBeNext from "./features/WantToBeNextSection/WantToBeNext";
 
 function App() {
   return (
     <AppLayout>
       <HeroSection />
-      <LatestWorkSection />
-      <ArtistProfileSection />
       <VideoLibrary />
-      {/* <AudioLibrary /> */}
-      <WantToBeNext />
+      <AudioLibrary />
       <MovieLibrary />
       <div className="border-b border-black w-full mb-10" /> {/* Divider */}
       <FeedbackAndMapSection />
