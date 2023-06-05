01aad1803c758ce43ba75447aebdf6940f0bd259
RajThapa7 <jyangoraj123@gmail.com>
2023-06-05 22:16:50 +0545

    merge issue fix

--- src/components/LatestWorkCard/LatestWorkCard.jsx
+++ src/components/LatestWorkCard/LatestWorkCard.jsx
@@ -1,5 +1,4 @@
 import classNames from "../../utils/classNames";
-import useFetchSpotifyLink from "./api/useFetchSpotifyLink";
 
 export default function LatestWorkCard({
   url,
@@ -8,32 +7,27 @@ export default function LatestWorkCard({
   setIsModalOpen,
   className,
 }) {
-  const isYoutubeLink = url.includes("youtube");
   const videoId = url.split("v=")[1];
 
-  const imageUrl = isYoutubeLink
-    ? `https://img.youtube.com/vi/${videoId}/0.jpg`
-    : url;
+  const imageUrl = `https://img.youtube.com/vi/${videoId}/0.jpg`;
 
   return (
     <div
       href="#"
       className={classNames(className, `group relative block overflow-hidden`)}
+      onClick={() => {
+        setIsModalOpen(true);
+        setSelectedVideo(videoId);
+      }}
     >
       <img
         src={imageUrl}
-        alt=""
-        className={`${
-          isYoutubeLink
-            ? "aspect-[16/9] object-cover"
-            : "aspect-[4/4] object-fill"
-        }
-         w-full  transition duration-500 group-hover:scale-105 `}
+        alt="img-latest-work"
+        className={`aspect-[16/9] object-cover w-full  transition duration-500 group-hover:scale-105 `}
       />
 
       <div className="relative bg-white py-2 flex flex-col items-center gap-y-1">
         <h3 className="text-xl font-semibold text-gray-900">{title}</h3>
-        {/* <p>{description}</p> */}
       </div>
     </div>
   );
