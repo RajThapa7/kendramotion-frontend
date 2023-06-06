e682cf151898f06585d2ec92dfec9c393ac947e6
RajThapa7 <jyangoraj123@gmail.com>
2023-06-06 09:19:18 +0545

    changes

--- src/api/createApi.js
+++ src/api/createApi.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const createApi = () => {
   // base url for the api
-  let url = import.meta.env.VITE_BASE_URL + "/api/v1";
+  // let url = import.meta.env.VITE_BASE_URL + "/api/v1";
+  let url = "https://kendramotion-backend.onrender.com/api/v1";
 
   const api = axios.create({
     baseURL: url,
